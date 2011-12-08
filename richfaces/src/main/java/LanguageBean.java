b90375556cb4e1654036cb6c9d99ee6e49c4d9b5
Valentin IONITA <valentin.ionita87@gmail.com>
2011-12-08 14:42:12 +0100

    story-4 fixed locale not being session scoped

--- richfaces/src/main/java/LanguageBean.java
+++ richfaces/src/main/java/LanguageBean.java
@@ -1,62 +1,59 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ValueChangeEvent;
-
-@ManagedBean(name="language")
+@ManagedBean(name = "language")
 @SessionScoped
-public class LanguageBean implements Serializable{
+public class LanguageBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private String localeCode;
 
-    private static Map<String,Object> countries;
-    static{
-        countries = new LinkedHashMap<String,Object>();
+    private static Map<String, Object> countries;
+
+    static {
+        countries = new LinkedHashMap<String, Object>();
         countries.put("English", Locale.ENGLISH); //label, value
         countries.put("Italian", Locale.ITALIAN);
+    }
+
+    public LanguageBean() {
+        localeCode = "en";
     }
 
     public Map<String, Object> getCountriesInMap() {
         return countries;
     }
 
-
     public String getLocaleCode() {
         return localeCode;
     }
-
 
     public void setLocaleCode(String localeCode) {
         this.localeCode = localeCode;
     }
 
-    public void countryLocaleCodeChanged(ValueChangeEvent e){
+    public void countryLocaleCodeChanged(ValueChangeEvent e) {
 
         String newLocaleValue = e.getNewValue().toString();
         changeLanguage(newLocaleValue);
     }
 
-    public void countryLocaleCodeChanged(){
-
+    public void countryLocaleCodeChanged() {
         String newLocaleValue = localeCode;
         changeLanguage(newLocaleValue);
     }
 
     private void changeLanguage(String newLocaleValue) {
-
         for (Map.Entry<String, Object> entry : countries.entrySet()) {
-
-            if(entry.getValue().toString().equals(newLocaleValue)) {
-
-                FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale)entry.getValue());
-
+            if (entry.getValue().toString().equals(newLocaleValue)) {
+                FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale) entry.getValue());
             }
         }
     }
