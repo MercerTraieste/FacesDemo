468247506a9d9a4a9cdc9d628ffc0db1f9519708
Valentin IONITA <valentin.ionita87@gmail.com>
2011-12-08 16:41:58 +0100

    story-4 cleanup faces-config

--- richfaces/src/main/java/RichBean.java
+++ richfaces/src/main/java/RichBean.java
@@ -1,5 +1,9 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 
+@ManagedBean
+@SessionScoped
 public class RichBean implements Serializable {
 
     private static final long serialVersionUID = -2403138958014741653L;
