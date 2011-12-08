4f070aed4c6a35a769d5c3b90a24aadaba23f410
Valentin IONITA <valentin.ionita87@gmail.com>
2011-12-08 16:40:54 +0100

    story-4 changed language codes

--- richfaces/src/main/java/LocaleBean.java
+++ richfaces/src/main/java/LocaleBean.java
@@ -1,10 +1,11 @@
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import java.io.Serializable;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 @ManagedBean
 @SessionScoped
@@ -14,16 +15,16 @@ public class LocaleBean implements Serializable {
 
     private String localeCode;
 
-    private Map<String, Object> languages;
+    private List<SelectItem> languages;
 
     public LocaleBean() {
-        languages = new LinkedHashMap<String, Object>();
-        languages.put("English", Locale.ENGLISH); //label, value
-        languages.put("Italian", Locale.ITALIAN);
-        localeCode = "en";
+        languages = new ArrayList<SelectItem>();
+        languages.add(new SelectItem("en_US", "English"));
+        languages.add(new SelectItem("it_IT", "Italian"));
+        localeCode = "it_IT";
     }
 
-    public Map<String, Object> getLanguages() {
+    public List<SelectItem> getLanguages() {
         return languages;
     }
 
@@ -35,17 +36,18 @@ public class LocaleBean implements Serializable {
         this.localeCode = localeCode;
     }
 
-    public void countryLocaleCodeChanged() {
-        String newLocaleValue = localeCode;
-        changeLanguage(newLocaleValue);
+    public void localeChanged() {
+        changeLocale();
     }
 
-    private void changeLanguage(String newLocaleValue) {
-        for (Map.Entry<String, Object> entry : languages.entrySet()) {
-            if (entry.getValue().toString().equals(newLocaleValue)) {
-                FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale) entry.getValue());
-            }
+    private void changeLocale() {
+        if ("en_US".equals(localeCode)) {
+            FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.ENGLISH);
+        } else if ("it_IT".equals(localeCode)) {
+            FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.ITALIAN);
+
+        } else {
+            throw new RuntimeException("Locale" + localeCode + " not supported");
         }
     }
-
 }
