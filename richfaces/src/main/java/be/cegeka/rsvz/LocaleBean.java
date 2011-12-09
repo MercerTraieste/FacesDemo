a967366385725239457b9ba100923aecbcb84aa4
Valentin IONITA <valentin.ionita87@gmail.com>
2011-12-09 11:29:23 +0100

    story-4 simplified localeBean

--- richfaces/src/main/java/be/cegeka/rsvz/LocaleBean.java
+++ richfaces/src/main/java/be/cegeka/rsvz/LocaleBean.java
@@ -1,15 +1,9 @@
 package be.cegeka.rsvz;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 @ManagedBean
@@ -18,44 +12,19 @@ public class LocaleBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private static final Locale EN_GR = new Locale("el","GR");
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
 
-    private static final Logger LOG = LoggerFactory.getLogger(LocaleBean.class);
-    private String localeCode;
-
-    private List<SelectItem> languages;
-
-    public LocaleBean() {
-        languages = new ArrayList<SelectItem>();
-        languages.add(new SelectItem("en_US", "English"));
-        languages.add(new SelectItem("el_GR", "Greek"));
-        localeCode = FacesContext.getCurrentInstance().getApplication().getDefaultLocale().toString();
-        LOG.info("Locale set to {}", localeCode);
+    public Locale getLocale() {
+        return locale;
     }
 
-    public List<SelectItem> getLanguages() {
-        return languages;
+    public String getLanguage() {
+        return locale.getLanguage();
     }
 
-    public String getLocaleCode() {
-        return localeCode;
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
     }
 
-    public void setLocaleCode(String localeCode) {
-        this.localeCode = localeCode;
-    }
-
-    public void localeChanged() {
-        changeLocale();
-    }
-
-    private void changeLocale() {
-        if ("en_US".equals(localeCode)) {
-            FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.ENGLISH);
-        } else if ("el_GR".equals(localeCode)) {
-            FacesContext.getCurrentInstance().getViewRoot().setLocale(EN_GR);
-        } else {
-            throw new RuntimeException("Locale" + localeCode + " not supported");
-        }
-    }
 }
