749bcaf77798410eae61492a92e079469384d3c2
Adelina Caramet <adelina.caramet@gmail.com>
2011-12-09 16:17:11 +0100

    Story 3 Icefaces cleanup

---
+++ icefaces/src/main/java/be/cegeka/rszv/faces/i18n/UTF8ResourceBundle.java
@@ -0,0 +1,82 @@
+package be.cegeka.rszv.faces.i18n;
+
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+/**
+ * ResourceBundle does not support utf-8 properties. Jsf 1.x/2.x do not bring any improvements. Java 1.6 does bring some
+ * improvements. Please read:
+ *
+ * TUTORIALS
+ * http://jdevelopment.nl/internationalization-jsf-utf8-encoded-properties-files/
+ * http://stackoverflow.com/questions/3645491/i18n-with-utf-8-encoded-properties-files-in-jsf-2-0-appliaction
+ * http://stackoverflow.com/questions/4659929/how-to-use-utf-8-in-resource-properties-with-resourcebundle
+ * http://balusc.blogspot.com/2009/05/unicode-how-to-get-characters-right.html
+ *
+ * REFERENCES
+ * http://www.columbia.edu/~fdc/utf8/
+ * http://www.loc.gov/standards/iso639-2/php/code_list.php
+ * http://www.iso.org/iso/support/country_codes/iso_3166_code_lists/iso-3166-1_decoding_table.htm
+ */
+public class UTF8ResourceBundle extends ResourceBundle {
+    protected static final String BUNDLE_NAME = "messages";
+    protected static final String BUNDLE_EXTENSION = "properties";
+    protected static final Control UTF8_CONTROL = new UTF8Control();
+
+    public UTF8ResourceBundle() {
+        setParent(ResourceBundle.getBundle(BUNDLE_NAME,
+                FacesContext.getCurrentInstance().getViewRoot().getLocale(), UTF8_CONTROL));
+    }
+
+    @Override
+    protected Object handleGetObject(String key) {
+        return parent.getObject(key);
+    }
+
+    @Override
+    public Enumeration getKeys() {
+        return parent.getKeys();
+    }
+
+    protected static class UTF8Control extends Control {
+        public ResourceBundle newBundle
+                (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+                throws IllegalAccessException, InstantiationException, IOException {
+            // The below code is copied from default Control#newBundle() implementation.
+            // Only the PropertyResourceBundle line is changed to read the file as UTF-8.
+            String bundleName = toBundleName(baseName, locale);
+            String resourceName = toResourceName(bundleName, BUNDLE_EXTENSION);
+            ResourceBundle bundle = null;
+            InputStream stream = null;
+            if (reload) {
+                URL url = loader.getResource(resourceName);
+                if (url != null) {
+                    URLConnection connection = url.openConnection();
+                    if (connection != null) {
+                        connection.setUseCaches(false);
+                        stream = connection.getInputStream();
+                    }
+                }
+            } else {
+                stream = loader.getResourceAsStream(resourceName);
+            }
+            if (stream != null) {
+                try {
+                    bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+                } finally {
+                    stream.close();
+                }
+            }
+            return bundle;
+        }
+    }
+
+}
