db7d1e0d3e07c1620c5c69de15857abbc62a9262
Adelina Caramet <adelina.caramet@gmail.com>
2011-12-13 16:02:48 +0100

    Story5 - Dynamic scrolling table Primefaces

--- primefaces/src/main/java/be/cegeka/rsvz/DataBean.java
+++ primefaces/src/main/java/be/cegeka/rsvz/DataBean.java
@@ -1,38 +1,59 @@
 package be.cegeka.rsvz;
 
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @ManagedBean(name = "dataBean")
 @SessionScoped
-public class DataBean {
+public class DataBean implements Serializable {
     
-    private int size;
-    private List<Child> children = new ArrayList<Child>();
+    private final int SIZE = 10;
+    private LazyDataModel<Child> children;
     
     public DataBean() {
-        setSize(100);
-        for (int i = 1; i < 1000; i++) {
-            children.add(new Child(String.valueOf(i), "child "+i+" fname", "child "+i+" lname", String.valueOf(i*10), "M"));
-        }
+        initLazyModel();
+    }
+    
+    public void initLazyModel() {
+        int count = 1000;
+        children = new LazyDataModel<Child>() {
+
+            @Override
+            public List<Child> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String,String> filters) {
+
+                List<Child> children = new ArrayList<Child>();
+                populateLazy(children, pageSize, first);
+
+                return children;
+            }
+        };
+        children.setRowCount(count);
+        children.setPageSize(SIZE);
     }
 
-    public List<Child> getChildren() {
+    public LazyDataModel<Child> getChildren() {
         return children;
     }
 
-    public void setChildren(List<Child> children) {
+    public void setChildren(LazyDataModel<Child> children) {
         this.children = children;
     }
 
-
     public int getSize() {
-        return size;
+        return SIZE;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    private void populateLazy(List<Child> children, int size, int first) {
+        for(int offset = first + 1 ; offset <= size ; offset++) {
+            children.add(new Child(String.valueOf(offset), "child "+offset+" fname", "child "+offset+" lname", String.valueOf(offset*10), "M"));
+        }
     }
+
 }
