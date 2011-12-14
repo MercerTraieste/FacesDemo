ec6ea8e4a796b0e573378da355f485d27f9e5ef8
Mercer Traieste <mercer.traieste@gmail.com>
2011-12-14 14:12:08 +0100

    story-4 testing that richfaces impl offers the browser language

--- richfaces/src/test/java/be/cegeka/rsvz/ui/FirstPageTest.java
+++ richfaces/src/test/java/be/cegeka/rsvz/ui/FirstPageTest.java
@@ -1,32 +1,35 @@
 package be.cegeka.rsvz.ui;
 
+import be.cegeka.rsvz.LocaleBean;
+import be.cegeka.rsvz.faces.i18n.UTF8ResourceBundle;
 import org.glassfish.embeddable.GlassFish;
 import org.glassfish.embeddable.GlassFishProperties;
 import org.glassfish.embeddable.GlassFishRuntime;
 import org.glassfish.embeddable.archive.ScatteredArchive;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.htmlunit.ExtendedHtmlUnitDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class FirstPageTest {
     private static final int PORT = 9999;
     private static final String CONTEXT = "facesdemo";
     private static final String BASE_URL = "http://localhost:" + PORT + "/" + CONTEXT;
+    private static final String[] LOCALES = {"en", "el", "fr", "nl", "en-GB"};
+
     private GlassFish glassfish;
     private WebDriver driver;
 
     @Before
     public void startServer() throws Exception {
-        driver = new HtmlUnitDriver();
-
         GlassFishProperties gfProps = new GlassFishProperties();
         gfProps.setPort("http-listener", PORT);
         glassfish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
@@ -36,19 +39,22 @@ public class FirstPageTest {
         ScatteredArchive archive = new ScatteredArchive(CONTEXT, ScatteredArchive.Type.WAR, webRoot);
         archive.addClassPath(classes);
         glassfish.getDeployer().deploy(archive.toURI());
+
+        driver = new ExtendedHtmlUnitDriver();
     }
 
     @Test
-    public void languageLabelShouldBeLanguage() {
-        driver.get(BASE_URL + "/");
-        WebElement element = driver.findElement(By.id("languageLabel"));
-        Assert.assertEquals("Language", element.getText());
-    }
-
-    @After
-    public void closeServer() throws Exception {
-        glassfish.stop();
-        glassfish.dispose();
+    public void allLocalesShouldBeAvailable() {
+        for (String locale : LOCALES) {
+            ExtendedHtmlUnitDriver localeDriver = new ExtendedHtmlUnitDriver();
+            localeDriver.setHeader("Accept-Language", locale);
+            localeDriver.get(BASE_URL);
+            WebElement element = localeDriver.findElement(By.id("languageLabel"));
+            Locale referenceLocale = LocaleBean.extractLocale(locale);
+            ResourceBundle res = UTF8ResourceBundle.getBundle("be.cegeka.rsvz.faces.i18n.messages", referenceLocale);
+            Assert.assertEquals(res.getString("language"), element.getText());
+            localeDriver.quit();
+        }
     }
 
     private String getLocalPath() throws IOException {
