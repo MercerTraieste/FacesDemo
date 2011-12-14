66caeada364364279f9c391adbce4f3c61852f21
Cosmin Ene <cosmin.ene@gmail.com>
2011-12-14 09:45:07 +0100

    story-5 Cleanup DataTable live scrolling Primefaces

--- primefaces/src/main/java/be/cegeka/rsvz/ScrollBean.java
+++ primefaces/src/main/java/be/cegeka/rsvz/ScrollBean.java
@@ -1,32 +1,27 @@
 package be.cegeka.rsvz;
 
-import org.primefaces.model.LazyDataModel;
-import org.primefaces.model.SortOrder;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @ManagedBean(name = "scrollBean")
 @SessionScoped
 public class ScrollBean implements Serializable {
 
-    private static final int SIZE = 10;
-    private int offset = 0;
-    private List<Child> children = new ArrayList<Child>(SIZE);
+    private static final int DISPLAY_SIZE = 10;
+    private static final int CAPACITY = 1000;
+    private List<Child> children = new ArrayList<Child>(DISPLAY_SIZE);
 
     public ScrollBean() {
-        loadNextBatch();
+        loadScrollableData();
     }
     
-    public void loadNextBatch() {
-        for (int i = offset + 1; i <= offset + SIZE; i++) {
-            children.add(new Child(String.valueOf(i), "child "+i+" fname", "child "+i+" lname", String.valueOf(i), "M"));
+    public void loadScrollableData() {
+        for (int i = 1; i <= CAPACITY; i++) {
+            children.add(new Child(i, "child "+i+" fname", "child "+i+" lname", i, "M"));
         }
-        offset += SIZE;
     }
 
     public List<Child> getChildren() {
@@ -36,13 +31,9 @@ public class ScrollBean implements Serializable {
     public void setChildren(List<Child> children) {
         this.children = children;
     }
-    
-    public int getListSize() {
-        return children.size();
-    }
 
     public int getSize() {
-        return SIZE;
+        return DISPLAY_SIZE;
     }
 
 }
