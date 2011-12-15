6e44c301df9c1b26a51850048a3baf979b1bacd9
Mercer Traieste <mercer.traieste@gmail.com>
2011-12-15 15:25:16 +0100

    story-4 added sorted list with locales example

--- richfaces/src/test/java/be/cegeka/rsvz/examples/GuavaTests.java
+++ richfaces/src/test/java/be/cegeka/rsvz/examples/GuavaTests.java
@@ -1,16 +1,37 @@
 package be.cegeka.rsvz.examples;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Ordering;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class GuavaTests {
 
     @Test
-    public void sortedList() {
-        List<String> locales = Arrays.asList("aaa", "ccc", "bbb");
-        System.out.println(Ordering.from(String.CASE_INSENSITIVE_ORDER).immutableSortedCopy(locales));
+    public void sortedStringList() {
+        List<String> localeCodes = Arrays.asList("nl", "en_GB", "en");
+        System.out.println(Ordering.from(String.CASE_INSENSITIVE_ORDER).immutableSortedCopy(localeCodes));
+    }
+
+    @Test
+    public void sortedLocaleList() {
+        List<Locale> locales = Arrays.asList(new Locale("nl"), new Locale("en", "GB"), new Locale("fr"));
+        Function<Locale, String> getNameFunction = new Function<Locale, String>() {
+            public String apply(Locale from) {
+                return from.toString();
+            }
+        };
+
+        Ordering<Locale> nameOrdering = Ordering.natural().onResultOf(getNameFunction);
+
+        ImmutableSortedSet<Locale> sortedLocales = ImmutableSortedSet.orderedBy(
+                nameOrdering).addAll(locales).build();
+        System.out.println(sortedLocales);
+
+
     }
 }
