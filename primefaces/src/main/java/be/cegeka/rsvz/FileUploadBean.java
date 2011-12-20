5e55aece144fd9a25a551112e42a0b2d20c2acef
Cosmin Ene <cosmin.ene@gmail.com>
2011-12-20 10:11:27 +0200

    story-8 upload file(s) for prime faces

---
+++ primefaces/src/main/java/be/cegeka/rsvz/FileUploadBean.java
@@ -0,0 +1,37 @@
+package be.cegeka.rsvz;
+
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+import java.io.Serializable;
+
+@ManagedBean(name = "fileBean")
+@SessionScoped
+public class FileUploadBean implements Serializable {
+
+    private UploadedFile file;
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void upload() {
+        FacesMessage msg = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    public void handleUpload(FileUploadEvent event) {
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+}
