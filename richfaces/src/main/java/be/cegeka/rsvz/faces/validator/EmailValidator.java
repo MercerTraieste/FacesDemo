21e0d4aa7c75f2f2a40c2610f6bc67a41afe9e1d
Adelina Caramet <adelina.caramet@gmail.com>
2011-12-21 17:00:21 +0200

    story-10 richfaces add resource bundle for custom validation messages

--- richfaces/src/main/java/be/cegeka/rsvz/faces/validator/EmailValidator.java
+++ richfaces/src/main/java/be/cegeka/rsvz/faces/validator/EmailValidator.java
@@ -2,22 +2,31 @@ package be.cegeka.rsvz.faces.validator;
 
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Scope;
+import java.util.ResourceBundle;
+
 
 @FacesValidator("emailValidator")
 public class EmailValidator implements Validator {
 
     private static final String EMAIL = "gigel.sparge@lemne.com";
 
+
     public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+        ResourceBundle bundle = ResourceBundle.getBundle("be.cegeka.rsvz.faces.i18n.messages", facesContext.getViewRoot().getLocale());
         if (o != null && EMAIL.equals(o.toString())) {
-            FacesMessage msg = new FacesMessage("Email validation failed. This address already exists.");
+            FacesMessage msg = new FacesMessage(bundle.getString("email-already-exists"));
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(msg);
         }
     }
+
 }
