11b1998d820a7d6f465b08d8d7ddc666e42f2bb7
Valentin IONITA <valentin.ionita87@gmail.com>
2011-12-22 16:19:15 +0200

    story-7 made last name mandatory

--- primefaces/src/main/java/be/cegeka/rsvz/RichBean.java
+++ primefaces/src/main/java/be/cegeka/rsvz/RichBean.java
@@ -1,18 +1,13 @@
 package be.cegeka.rsvz;
 
 import be.cegeka.rsvz.validator.Insz;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-import javax.faces.event.AjaxBehaviorEvent;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -21,9 +16,9 @@ import java.util.Date;
 public class RichBean implements Serializable {
 
     private static final Logger LOG = LoggerFactory.getLogger(RichBean.class);
-    @NotNull
-    @Size(min = 4, max = 10, message = "{must-not-be-empty}")
+
     private String firstName;
+    @NotEmpty(message = "{must-not-be-empty}")
     private String lastName;
     @Past(message = "{date-must-be-in-the-past}")
     private Date calendar;
@@ -32,14 +27,6 @@ public class RichBean implements Serializable {
 
     @Insz(message = "{insz-not-valid}")
     private String insz;
-
-    public void validateFirstName(AjaxBehaviorEvent event) {
-        System.out.println("In validateFirstName ActionListener.");
-        System.out.println("First Name: " + getFirstName());
-        System.out.println("LastName: " + getLastName());
-        FacesMessage msg = new FacesMessage("Succesful is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(event.getComponent().getClientId(), msg);
-    }
 
     public String getFirstName() {
         return firstName;
