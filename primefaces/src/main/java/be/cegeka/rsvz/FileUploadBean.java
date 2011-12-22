26b8464f6971169463751163974bde9afd74d837
Valentin IONITA <valentin.ionita87@gmail.com>
2011-12-22 17:24:04 +0200

    story-7 removed simple file upload component

--- primefaces/src/main/java/be/cegeka/rsvz/FileUploadBean.java
+++ primefaces/src/main/java/be/cegeka/rsvz/FileUploadBean.java
@@ -1,15 +1,12 @@
 package be.cegeka.rsvz;
 
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
-
 import java.io.Serializable;
 
 @ManagedBean(name = "fileBean")
@@ -26,10 +23,6 @@ public class FileUploadBean implements Serializable {
         this.file = file;
     }
 
-    public void upload() {
-        FacesMessage msg = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
     public void handleUpload(FileUploadEvent event) {
         FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, msg);
